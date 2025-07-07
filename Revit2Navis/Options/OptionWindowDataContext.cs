82fa14f822d1010d775d04686e84213abfaa3eac
Sergey Shevtsov <sergey.shevtsov@xyicon.com>
2025-07-07 12:02:43 +0600

    Restore button update;

--- Revit2Navis/Options/OptionWindowDataContext.cs
+++ Revit2Navis/Options/OptionWindowDataContext.cs
@@ -4,9 +4,26 @@ using System.Windows.Input;
 namespace Revit2Navis.Options;
 public class OptionWindowDataContext
 {
-    public OptionWindowDataContext()
-    {
-    }
+    public ICommand RestoreCommand => new RelayCommand<Window>(
+      window =>
+      {
+          Properties.Settings.Default.ExportParts = false;
+          Properties.Settings.Default.ExportElementIds = true;
+          Properties.Settings.Default.NavisworksParameter = "All";
+          Properties.Settings.Default.ConvertElementProperties = false;
+          Properties.Settings.Default.ExportLinks = true;
+          Properties.Settings.Default.ExportRoomAsAttribute = true;
+          Properties.Settings.Default.ExportUrls = true;
+          Properties.Settings.Default.NavisworksCoordinate = "Internal";
+          Properties.Settings.Default.DivideFileIntoLevels = false;
+          Properties.Settings.Default.NavisworksExportScope = "Model";
+          Properties.Settings.Default.ExportRoomGeometry = true;
+          Properties.Settings.Default.FindMissingMaterials = true;
+          Properties.Settings.Default.Save();
+          window?.Close();
+      });
+
+
     public ICommand CloseCommand => new RelayCommand<Window>(
         window =>
         {
